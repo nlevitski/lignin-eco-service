fc83c2823e7892d6b697958b688391befa1d91f1
nlevitski <reflevius@gmail.com>
2025-09-09 23:01:38 +0300

    fix: config

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,8 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+	host: env('HOST', '0.0.0.0'),
+	port: env.int('PORT', 1337),
+	url: env('PUBLIC_URL', 'https://lignineco.com/api'),
+	app: {
+		keys: env.array('APP_KEYS'),
+	},
 });
