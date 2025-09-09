b695d57cede94f4c1a6f29fff600144c4473906e
nlevitski <reflevius@gmail.com>
2025-09-10 00:25:30 +0300

    fix: config vite

---
+++ src/admin/vite.config.ts
@@ -0,0 +1,16 @@
+import { server } from '@strapi/strapi/admin/test';
+import { mergeConfig, type UserConfig } from 'vite';
+
+export default (config: UserConfig) => {
+	// Important: always return the modified config
+	return mergeConfig(config, {
+		resolve: {
+			alias: {
+				'@': '/src',
+			},
+		},
+		server: {
+			allowedHosts: true,
+		},
+	});
+};
